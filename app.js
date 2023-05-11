cd76eb34ffc6766dbbdf7394f984e816ad72137b
msa8wsy <msa8wsy@virginia.edu>
2023-05-11 11:08:22 -0400

    Changed sdk usage

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require("express");
+const AWS = require("aws-sdk")
 const app = express();
 const port = process.env.PORT || 3001;
-const LAMBDA_URL = process.env.LAMBDA_URL
+const lambda = new AWS.Lambda({region: 'us-east-1'})
 
 // app.get("/", (req, res) => res.type('html').send(html));
 app.get("/", async (req, res) => {
-  try {
-    let response = await fetch(LAMBDA_URL, {
-      method: 'GET'
-    })
-    let jsonResponse = await response.json()
-    res.status(200).json(response)
-  } catch (error) {
-    res.status(400).json(error)
-  }
+  lambda.invoke((error, data) => {
+    if (error != null || error != undefined) {
+      res.status(400).json(error)
+    } else {
+      res.status(200).json(data)
+    }
+  })
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
