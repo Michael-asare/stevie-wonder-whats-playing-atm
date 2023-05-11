69f7bcc280c91b06900d6e76a81c812f22f4c490
msa8wsy <msa8wsy@virginia.edu>
2023-05-11 16:26:55 -0400

    Fixed request

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ const AWS = require("aws-sdk");
 const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
 const app = express();
 const port = process.env.PORT || 3001;
+process.env.AWS_ACCESS_KEY_ID = "AKIASRKSLMKBPIBZCVX2"
+process.env.AWS_SECRET_ACCESS_KEY = "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"
 const client = new LambdaClient({
   region: 'us-east-1'
 })
 const command = new InvokeCommand({
   FunctionName: 'steviewonder-get-currently-playing',
   InvocationType: 'RequestResponse',
-  Payload: 'String'
+  Payload: JSON.stringify('') // here holds the json key value pairing for the request (nothing is needed atm)
 })
 
-// app.get("/", (req, res) => res.type('html').send(html));
 app.get("/", async (req, res) => {
   try {
     const response = await client.send(command)
+    console.log(response)
     if (response.FunctionError) {
       res.status(400).json(response.FunctionError)
     } else {
-      res.status(response.StatusCode).json(response.Payload)
+      const result = Buffer.from(response.Payload).toString()
+      const jsonResult = JSON.parse(result)
+      res.status(response.StatusCode).json(jsonResult)
     }
   } catch (error) {
     res.status(400).json(error)
