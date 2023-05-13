fdb66e5a47a7e001f440e7fdb42b6a72fa8c678a
msa8wsy <msa8wsy@virginia.edu>
2023-05-13 03:38:45 -0400

    Integrated a new endpoint for colors retrieval

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const AWS = require("aws-sdk");
 const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
 const app = express();
 const port = process.env.PORT || 3001;
+const fetch = require("node-fetch")
 const client = new LambdaClient({
   region: 'us-east-1'
 })
@@ -28,4 +29,25 @@ app.get("/current", async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const colorsEndpoint = "https://us-east4-stevie-wonder-386422.cloudfunctions.net/song-colors"
+app.get("/current/colors", async (req, res) => {
+  let numClusters = req.query.numClusters;
+  try {
+    const queryParams = numClusters == undefined ? "" : `numClusters=${numClusters}`
+    const response = await fetch(colorsEndpoint+queryParams, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'}
+    })
+    console.log(response)
+    if (response.ok) {
+      res.status(200).json(response.body)
+    } else {
+      console.log("Error arrived during song cover -> colors process")
+      res.status(400).json(response.body)
+    }
+  } catch (error) {
+    console.log("Error arrived early")
+    res.status(400).json(error)
+  }
+});
+app.listen(port, () => console.log(`Stevie Wonder app listening on port ${port}!`));
