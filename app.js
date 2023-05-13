71e13801d2e610d3aa7aa92d35c8517bf52abec2
msa8wsy <msa8wsy@virginia.edu>
2023-05-13 06:28:58 -0400

    Included google auth info

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const AWS = require("aws-sdk");
 const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
 const app = express();
 const port = process.env.PORT || 3001;
-const client = new LambdaClient({
+const lambdaClient = new LambdaClient({
   region: 'us-east-1'
 })
 const command = new InvokeCommand({
@@ -12,9 +11,11 @@ const command = new InvokeCommand({
   Payload: JSON.stringify('') // here holds the json key value pairing for the request (nothing is needed atm)
 })
 
+
+
 app.get("/current", async (req, res) => {
   try {
-    const response = await client.send(command)
+    const response = await lambdaClient.send(command)
     console.log(response)
     if (response.FunctionError) {
       res.status(400).json(response.FunctionError)
@@ -29,17 +30,35 @@ app.get("/current", async (req, res) => {
 });
 
 const colorsEndpoint = "https://us-east4-stevie-wonder-386422.cloudfunctions.net/song-colors"
+
+const { GoogleAuth } = require('google-auth-library')
+const googleAuth = new GoogleAuth()
+
+
 app.get("/current/colors", async (req, res) => {
   console.log("Got in a request for /current/colors")
   console.log(req)
   let numClusters = req.query.numClusters;
   try {
+    console.log("Attempting to create an authenticated google request client...")
+    const googleAuthClient = await googleAuth.getIdTokenClient(colorsEndpoint)
+    console.log("...done")
     const queryParams = numClusters == undefined ? "" : `?numClusters=${numClusters}`
-    console.log(`Attempting to ping ${colorsEndpoint+queryParams}`)
-    const response = await fetch(colorsEndpoint+queryParams, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json'}
+    console.log(`Attempting to ping ${colorsEndpoint + queryParams}`)
+
+    const response =  await googleAuthClient.request({
+      "url": colorsEndpoint + queryParams,
+      "method": "POST",
+      "headers": {
+        "Content-Type": "application/json"
+      }
     })
+    // const response = await fetch(colorsEndpoint + queryParams, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   }
+    // })
     const jsonResponse = await response.json()
     console.log(`Ping successful! Here is the response in json form: ${JSON.stringify(jsonResponse, null, 4)}`)
     if (response.ok) {
